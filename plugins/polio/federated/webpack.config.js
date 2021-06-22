a32ba0c608906436dd09a4c9648eb93de6109f29
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-06-22 14:24:00 +0200

    webpack config for module federation

---
+++ plugins/polio/federated/webpack.config.js
@@ -0,0 +1,115 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index.bundle.js',
+    },
+    devServer: {
+        port: 3001,
+        watchContentBase: true,
+        historyApiFallback: true,
+        open: true,
+        openPage: 'polio',
+        proxy: {
+            '/api': 'http://localhost:8081',
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'react-hot-loader/webpack' },
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            configFile: './.babelrc',
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+            },
+            // font files
+            {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    mimetype: 'application/font-woff',
+                },
+            },
+            {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    mimetype: 'application/font-woff',
+                },
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    mimetype: 'application/octet-stream',
+                },
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file-loader',
+            },
+            // images
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    mimetype: 'image/svg+xml',
+                },
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 8192,
+                },
+            },
+            // videos
+            {
+                test: /\.mp4$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    mimetype: 'video/mp4',
+                },
+            },
+            // Leaftlet images
+            {
+                test: /\.png(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    mimetype: 'image/png',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'polio',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './config': '../js/src/config',
+            },
+            shared: [],
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+};
